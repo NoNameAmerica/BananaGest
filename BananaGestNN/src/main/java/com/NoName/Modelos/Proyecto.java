f8973838bc8599d65e5a5e2037f5839862b9a1ec
Gabrytron <gabbarceb@gmail.com>
2017-05-30 19:43:43 +0200

    objeto proyecto

---
+++ BananaGestNN/src/main/java/com/NoName/Modelos/Proyecto.java
@@ -0,0 +1,16 @@
+package com.NoName.Modelos;
+
+public class Proyecto {
+	protected int idProyecto;
+	public String titulo;
+	public String descripcion;
+	public String fechaIni;
+	public String fechaFin;
+	public int idUsuario;
+
+	public Proyecto() {
+		
+	}
+
+
+}
