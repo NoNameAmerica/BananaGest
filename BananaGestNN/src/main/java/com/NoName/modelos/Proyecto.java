db2d915aeb10f4f4a61a6c040f4fda4cc2e487be
Gabrytron <gabbarceb@gmail.com>
2017-05-30 19:55:51 +0200

    Objeto Proyecto

---
+++ BananaGestNN/src/main/java/com/NoName/modelos/Proyecto.java
@@ -0,0 +1,65 @@
+package com.NoName.modelos;
+
+public class Proyecto {
+	protected int idProyecto;
+	public String titulo;
+	public String descripcion;
+	public int getIdProyecto() {
+		return idProyecto;
+	}
+
+	public void setIdProyecto(int idProyecto) {
+		this.idProyecto = idProyecto;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getFechaIni() {
+		return fechaIni;
+	}
+
+	public void setFechaIni(String fechaIni) {
+		this.fechaIni = fechaIni;
+	}
+
+	public String getFechaFin() {
+		return fechaFin;
+	}
+
+	public void setFechaFin(String fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String fechaIni;
+	public String fechaFin;
+	public int idUsuario;
+
+	public Proyecto() {
+		
+	}
+
+	
+
+}
