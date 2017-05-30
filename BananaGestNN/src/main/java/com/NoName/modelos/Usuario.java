117ff8ae77356507e517452e8e3ff6ea0b720fc7
Arismar <arismarsampaio@gmail.com>
2017-05-30 20:01:47 +0200

    Usuario

---
+++ BananaGestNN/src/main/java/com/NoName/modelos/Usuario.java
@@ -0,0 +1,13 @@
+package com.NoName.modelos;
+
+public class Usuario {
+	protected int IdUser;
+	public String Nombre;
+	public String Apellido_Primero;
+	public String Apellido_Segundo;
+	public String Email;
+	protected String contarseña;
+	public String Foto;
+	public String Video;
+
+}
