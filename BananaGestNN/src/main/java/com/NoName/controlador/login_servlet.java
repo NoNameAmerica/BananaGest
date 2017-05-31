42364fee9608f8783e09dcf9335d8f8caeac641b
GABAR <GABAR@GABAR-PC>
2017-05-31 12:03:28 +0200

    login.html, Imgenes y css

--- BananaGestNN/src/main/java/com/NoName/controlador/login_servlet.java
+++ BananaGestNN/src/main/java/com/NoName/controlador/login_servlet.java
@@ -1,20 +1,73 @@
+/**
+ * @author: Arismar y gabriel
+ */
 package com.NoName.controlador;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.NoName.modelos.Usuario;
 
 public class login_servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	private int userId = 0;
+
+	public login_servlet() {
+		super();
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("email", "");
+		request.setAttribute("pass", "");
+		doPost(request, response);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Recojo los valores del formulario que viene en request del metodo get().
+		String user = request.getParameter("email");
+		String pwd = request.getParameter("pass");
+		Usuario miUser = new Usuario();
+		// Si es usuario abrir sesion
+
+		if (EsUsuario(user, pwd)) {
+			// Crear una sesion
+			HttpSession misession = request.getSession(true);
+			misession.setAttribute("user", userId);
+			request.getRequestDispatcher("login.jsp");
+			//RequestDispatcher rd = getServletContext().getRequestDispatcher("login.jsp");
+			// response.getWriter().append("Served at: ").append(request.getContextPath());
+		} else {
+			// Indicar el error y recargar el login
+			RequestDispatcher rd = getServletContext().getRequestDispatcher("login.jsp");
+		}
+	}
+
+	private boolean EsUsuario(String email, String password) {
+		Pattern regex = Pattern.compile("\\b[\\w.%-]+@[-.\\w]+\\.[A-Za-z]{2,4}\\b");
+		Matcher matcher = regex.matcher(email);
+		if (matcher.matches()){
+			if (email.equals("Ricardo@j.es") && password.equals("ricard")) {
+				userId = 1;
+				return true;
+			}
+			if (email.equals("Juana@j.es") && password.equals("juana")) {
+				userId = 2;
+				return true;
+			}
+			if (email.equals("luis@j.es") && password.equals("lluis")) {
+				userId = 3;
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
---
+++ BananaGestNN/src/main/webapp/login.jsp
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Acceso a los usuarios de bananagest">
+    <meta name="keywords" content="HTML,CSS,XML,JavaScript">
+    <meta name="author" content="Equipo NoName: Arismar y Gabriel">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Banana Gest</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="estilo/style.css">
+</head>
+
+<body>
+    <header>
+        <h1>Banana Gest</h1>
+    </header>
+    <section>
+        <p>Acceso de Usuario</p>
+        <form id="formlogin" action="bananagest.html" method="GET" novalidate>
+
+            <div>
+                <span id="err_1" class="oculto error">El formato del email no es correcto</span>
+                <span id="err_2" class="oculto error">El password es demasiado corto</span>
+                <span id="err_3" class="oculto error">Introduce el email y password</span>
+            </div>
+
+            <div class="form-input">
+                <label id="emailLb" for="email">Email</label>
+                <input id="email" type="text" placeholder="Email" name="email" pattern="\S+@\S+\.\S+" required/>
+            </div>
+            <div class="form-input">
+                <label id="passLb" for="pass">Password</label>
+                <input id="pass" type="password" placeholder="Contraseña" name="pass" minlength="6" required/>
+            </div>
+
+            <div class="form-input-accept">
+                <span><a>¿Recuperar Contraseña?</a></span>
+            </div>
+
+            <div class="boton-inf">
+                <input id="boton_entrar" type="submit" value="Iniciar Secion" />
+            </div>
+        </form>
+    </section>
+
+</body>
+<script src="js/login.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="bootstrap-3.3.7-dist\js\bootstrap.js"></script>
+
+</html>
