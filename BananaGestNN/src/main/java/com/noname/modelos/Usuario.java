226ddb3c45f86704337e5fb1d7c025d2eb99d023
A4Alumno <A4Alumno@PCAULA169.aules.local>
2017-05-31 19:56:36 +0200

    Modificación usuario

--- BananaGestNN/src/main/java/com/noname/modelos/Usuario.java
+++ BananaGestNN/src/main/java/com/noname/modelos/Usuario.java
@@ -8,61 +8,132 @@ import java.util.regex.Pattern;
 
 public class Usuario {
 	protected int idUser;
-	public String Nombre;
-	public String apellido_Primero;
-	public String apellido_Segundo;
-	public String email;
-	protected String contarseña;
-	public String foto;
-	public String video;
+	private String nombre;
+	private String apellido_primero;
+	private String apellido_segundo;
+	private String email;
+	protected String contraseña;
+	private String foto;
+	private String video;
 
-	//Construcctor para usuario logeandose
+	// Construcctor para usuario logeandose
 	public Usuario(String email, String contarseña) {
 		super();
-		this.idUser=0;
-		this.Nombre="";
-		this.apellido_Primero="";
-		this.apellido_Segundo="";
-		this.foto="";
-		this.video="";
+		this.idUser = 0;
+		this.nombre = "";
+		this.apellido_primero = "";
+		this.apellido_segundo = "";
+		this.foto = "";
+		this.video = "";
 		this.email = email;
-		this.contarseña = contarseña;
+		this.contraseña = contraseña;
 	}
 
+	public int getIdUser() {
+		return idUser;
+	}
+
+	private void setIdUser(int idUser) {
+		this.idUser = idUser;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public boolean setNombre(String nombre) {
+		Pattern pat = Pattern.compile(".*abc.*");
+	     Matcher mat = pat.matcher(nombre);
+	     if (mat.matches()) {
+	    	 this.nombre = nombre;
+	    	 return true;
+	     } else {
+	    	 this.nombre = "";
+	    	 return false;
+	     }		
+	}
+
+	public String getApellido_Primero() {
+		return apellido_primero;
+	}
+
+	public void setApellido_Primero(String apellido_Primero) {
+		this.apellido_primero = apellido_primero;
+	}
+
+	public String getapellido_primero() {
+		return apellido_segundo;
+	}
+
+	public void setApellido_Segundo(String apellido_Segundo) {
+		this.apellido_segundo = apellido_Segundo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	private String getContarseña() {
+		return contraseña;
+	}
+
+	public void setContarseña(String contarseña) {
+		this.contraseña = contarseña;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public String getVideo() {
+		return video;
+	}
+
+	public void setVideo(String video) {
+		this.video = video;
+	}
 
 	public boolean EsUsuario(String email, String password) {
 		Pattern regex = Pattern.compile("\\b[\\w.%-]+@[-.\\w]+\\.[A-Za-z]{2,4}\\b");
 		Matcher matcher = regex.matcher(email);
-		if (matcher.matches()){
+		if (matcher.matches()) {
 			if (email.equals("Ricardo@j.es") && password.equals("ricard")) {
 				this.idUser = 1;
-				this.Nombre="Ricardo";
-				this.apellido_Primero="Tejero";
-				this.apellido_Segundo="Sanchez";
-				this.foto="/img/edwin.jpg";
-				this.video="";
+				this.nombre = "Ricardo";
+				this.apellido_primero = "Tejero";
+				this.apellido_segundo = "Sanchez";
+				this.foto = "/img/edwin.jpg";
+				this.video = "";
 				return true;
 			}
 			if (email.equals("Juana@j.es") && password.equals("juana")) {
 				this.idUser = 2;
-				this.Nombre="Juana";
-				this.apellido_Primero="Gutierrez";
-				this.apellido_Segundo="";
-				this.foto="/img/gabriel.jpg";
-				this.video="";
+				this.nombre = "Juana";
+				this.apellido_primero = "Gutierrez";
+				this.apellido_segundo = "";
+				this.foto = "/img/gabriel.jpg";
+				this.video = "";
 				return true;
 			}
 			if (email.equals("luis@j.es") && password.equals("lluis")) {
 				this.idUser = 3;
-				this.Nombre="Luis";
-				this.apellido_Primero="Fernanded";
-				this.apellido_Segundo="";
-				this.foto="/img/aismar.jpg.jpg";
-				this.video="";			
+				this.nombre = "Luis";
+				this.apellido_primero = "Fernanded";
+				this.apellido_segundo = "";
+				this.foto = "/img/aismar.jpg.jpg";
+				this.video = "";
 				return true;
 			}
 		}
 		return false;
 	}
-	
+
 }
